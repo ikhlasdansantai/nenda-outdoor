475cd509da9d8896501c17a96cb7b5b4ead3a881
ikhlasdansantai <initesting5@gamil.com>
2023-07-26 11:01:36 +0700

    debug script, change script path

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('[data-collapse-toggle="navbar-default"]');
+const navbar = document.getElementById("navbar-default");
+
+button.addEventListener("click", () => {
+  const expanded = button.getAttribute("aria-expanded") === "true" || false;
+  button.setAttribute("aria-expanded", !expanded);
+  navbar.classList.toggle("hidden");
+});
+
+console.log("Script Run");
